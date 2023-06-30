c8e490a65149a647631b84244cb4973037581e6c
Kyohei Uto [Copilot] <h27.kubb@gmail.com>
2023-06-30 12:25:37 +0900

    neovimn透過設定を追加

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -3,3 +3,11 @@ if not vim.g.vscode then
     -- vim.cmd("colorscheme iceberg")
     -- vim.cmd("colorscheme catppuccin-mocha") --catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 end
+
+-- 背景を透過可能にする
+vim.cmd[[highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight NonText guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight LineNr guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight Folded guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight EndOfBuffer guibg=NONE ctermbg=NONE]]
+vim.cmd[[highlight SignColumn guibg=NONE ctermbg=NONE]]
--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -4,12 +4,15 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
 local telescope = require('telescope')
 telescope.setup {
+  defaults = {
+    winblend = 10
+  },
   pickers = {
     find_files = {
       hidden = true,
@@ -18,9 +21,9 @@ telescope.setup {
     live_grep = {
       initial_mode = "normal",
     },
-    -- buffers = {
-    --   initial_mode = "normal",
-    -- },
+    buffers = {
+      initial_mode = "normal",
+    },
     oldfiles = {
       initial_mode = "normal",
     },
