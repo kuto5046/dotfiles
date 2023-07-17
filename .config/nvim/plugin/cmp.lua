aa5eeec71d1dc5e4977f3dc8fb2a61c3ba28d460
Kyohei Uto <h27.kubb@gmail.com>
2023-07-17 10:57:22 +0900

    lsp系を簡略化

--- .config/nvim/plugin/cmp.lua
+++ .config/nvim/plugin/cmp.lua
@@ -1,42 +1,42 @@
--- local status, cmp = pcall(require, "cmp")
--- if (not status) then return end
-
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+local cmp = require("cmp")
 
 cmp.setup({
-snippet = {
-    expand = function(args)
-    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-},
-window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-}, {
-    { name = 'buffer' },
-})
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+	  },
+    {
+		  { name = "buffer" },
+	  })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-}, {
-    { name = 'buffer' },
-})
-})
+  sources = cmp.config.sources(
+  {
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  },
+  {
+      { name = 'buffer' },
+  })
+  })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
@@ -54,4 +54,19 @@ sources = cmp.config.sources({
 }, {
     { name = 'cmdline' }
 })
+})
+
+-- Set up lspkind.
+local lspkind = require("lspkind")
+cmp.setup({
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol",
+			maxwidth = 50,
+			ellipsis_char = "...",
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
 })
--- .config/nvim/plugin/mason.lua
+++ .config/nvim/plugin/mason.lua
@@ -1,45 +1,15 @@
--- local status, mason = pcall(require, "mason")
--- if (not status) then return end
--- local on_attach = function(client, bufnr)
-
 require('mason').setup()
-require('mason-lspconfig').setup_handlers({ function(server)
-  local opt = {
-    on_attach = function(client, bufnr)
-      local opts = { noremap=true, silent=true }
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.format {async=true}<CR>', opts)
-
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-
-      vim.cmd [[
-      let s:bl = ['json'] " set blacklist filetype
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold,CursorHoldI <buffer> if index(s:bl, &ft) < 0 | lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved,CursorMovedI <buffer> if index(s:bl, &ft) < 0 | lua vim.lsp.buf.clear_references()
-      augroup END
-      ]]
-    end,
-    capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    "pyright",
+    "rust_analyzer",
+    "lua_ls",
   }
-  require('lspconfig')[server].setup(opt)
-end })
+})
 
 -- After setting up mason-lspconfig you may set up servers via lspconfig
-require("lspconfig")["rust_analyzer"].setup {
-  on_attach = on_attach
-}
+require("lspconfig")["lua_ls"].setup({})
+require("lspconfig")["rust_analyzer"].setup({})
 
 -- poetryに対応
 require("lspconfig")["pyright"].setup {
