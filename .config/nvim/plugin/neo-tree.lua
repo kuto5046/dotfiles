a183e66b55b70394fac95a5a263b1fb69fd33d77
Kyohei Uto <h27.kubb@gmail.com>
2023-11-19 11:35:17 +0900

    Fix Neo-tree setup and keymap

--- .config/nvim/plugin/neo-tree.lua
+++ .config/nvim/plugin/neo-tree.lua
@@ -1,12 +1,14 @@
-require("neo-tree").setup({
-  enable_git_status = true,
-  enable_diagnostics = true,
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
-      hide_gitignored = false,
+if not vim.g.vscode then
+  require("neo-tree").setup({
+    enable_git_status = true,
+    enable_diagnostics = true,
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      }
     }
-  }
-})
-vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<Return>', {noremap = true})
+  })
+  vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<Return>', {noremap = true})  
+end
 
