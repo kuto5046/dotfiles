d6afa909dbff9fcf8695da57ecdf235eb37715c0
Kyohei Uto <h27.kubb@gmail.com>
2023-12-19 08:49:35 +0900

    don't use neo-tree in vscode

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,30 @@
-return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	keys = {
-		{ "<C-b>", ":Neotree toggle<Return>", desc = "toggle neo-tree" },
-	},
-	config = function()
-		require("neo-tree").setup({
-			enable_git_status = true,
-			enable_diagnostics = true,
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
+if vim.g.vscode then
+	return {}
+else
+	return {
+
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{ "<C-b>", ":Neotree toggle<Return>", desc = "toggle neo-tree" },
+		},
+		config = function()
+			require("neo-tree").setup({
+				enable_git_status = true,
+				enable_diagnostics = true,
+				filesystem = {
+					filtered_items = {
+						hide_dotfiles = false,
+						hide_gitignored = false,
+					},
 				},
-			},
-		})
-	end,
-}
+			})
+		end,
+	}
+end
