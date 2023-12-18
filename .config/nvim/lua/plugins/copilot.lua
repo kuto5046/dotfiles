3925b1bcb29705ae3bbf78860b81d103f5afa591
Kyohei Uto <h27.kubb@gmail.com>
2023-12-18 23:41:46 +0900

    add copilot status

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -15,4 +15,22 @@ return {
 			require("copilot_cmp").setup({})
 		end,
 	}, -- copilot completions
+	{
+		"jonahgoldwastaken/copilot-status.nvim",
+		dependencies = { "zbirenbaum/copilot.lua" },
+		config = function()
+			require("copilot_status").setup({
+				icons = {
+					idle = "",
+					error = " ",
+					offline = " ",
+					warning = "𥉉",
+					loading = " ",
+				},
+				debug = false,
+			})
+		end,
+		-- lazy = true,
+		-- event = "BufReadPost",
+	}, -- copilot status
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,5 +2,24 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = { "BufNewFile", "BufRead" },
-	config = 'require("lualine").setup()',
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					"encoding",
+					"fileformat",
+					"filetype",
+					-- copilot status
+					{
+						function()
+							return require("copilot_status").status_string()
+						end,
+						cnd = function()
+							return require("copilot_status").enabled()
+						end,
+					},
+				},
+			},
+		})
+	end,
 }
