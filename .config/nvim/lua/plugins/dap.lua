f6d35598fec2303cc3b19e4dea38086b7d564e52
Kyohei Uto <h27.kubb@gmail.com>
2024-01-03 17:46:16 +0900

    change breakpoint icon

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -17,6 +17,17 @@ return {
 		"rcarriga/nvim-dap-ui",
 		config = function()
 			require("dapui").setup({})
+			vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
+			vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", culhl = "DapStopped" })
+			vim.fn.sign_define("DapBreakooibtCondition", { text = "", texthl = "DapBreakpoint" })
+			vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint" })
+			vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint" })
+			-- 赤色にする
+			vim.cmd([[highlight DapBreakpoint guifg=#993939]])
+			vim.cmd([[highlight DapStopped guifg=#993939 ]])
+			vim.cmd([[highlight DapBreakooibtCondition guifg=#993939]])
+			vim.cmd([[highlight DapLogPoint guifg=#993939]])
+			vim.cmd([[highlight DapBreakpointRejected guifg=#993939]])
 		end,
 	},
 	-- dap-uiで型チェックを有効にするために推奨されている
