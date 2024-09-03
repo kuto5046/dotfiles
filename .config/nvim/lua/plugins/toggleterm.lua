68b56af54cf28ee778856a224995a900d52eb9ff
Kyohei Uto <h27.kubb@gmail.com>
2024-09-03 19:01:31 +0900

    toggle termの見た目を変更

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,10 +3,10 @@ return {
 	event = "BufRead",
 	config = function()
 		require("toggleterm").setup({
-			open_mapping = [[<c-`>]], -- vscodeのkeybindに合わせる
+			open_mapping = [[<c-t>]], -- vscodeのkeybindに合わせる
 			start_in_insert = true,
 			insert_mappings = false, -- insert時はmapping適用しない(文字入力時にスペースの入力が遅くなるので)
-			direction = "horizontal",
+			direction = "float",
 			shade_terminals = false,
 		})
 	end,
