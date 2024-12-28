fd27bfc801fd3078011704eefcbb3756dd01e463
Kyohei Uto <h27.kubb@gmail.com>
2024-12-28 14:27:27 +0900

    ステータスバーにLSPを表示

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,9 +3,29 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = { "BufNewFile", "BufRead" },
 	config = function()
+		local lsp_names = function()
+			local clients = {}
+			for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+				if client.name == "null-ls" then
+					local sources = {}
+					for _, source in ipairs(require("null-ls.sources").get_available(vim.bo.filetype)) do
+						table.insert(sources, source.name)
+					end
+					table.insert(clients, "null-ls(" .. table.concat(sources, ", ") .. ")")
+				else
+					table.insert(clients, client.name)
+				end
+			end
+			return " " .. table.concat(clients, ", ")
+		end
+
 		require("lualine").setup({
 			sections = {
+				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_x = {
+					lsp_names,
+				},
+				lualine_y = {
 					"encoding",
 					"fileformat",
 					"filetype",
