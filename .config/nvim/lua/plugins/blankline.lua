b5eba19a5d55c949b1533655945448dfc4a741fb
Kyohei Uto <h27.kubb@gmail.com>
2024-12-28 13:14:41 +0900

    透明化を再設定

--- .config/nvim/lua/plugins/blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- 派手なので無効化
-	-- "lukas-reineke/indent-blankline.nvim",
-	-- main = "ibl",
-	-- opts = {},
-	-- config = function()
-	--   require("ibl").setup({})
-	-- end,
-}
---
+++ .config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,20 @@
+return {
+	"xiyaowong/transparent.nvim",
+	config = function()
+		require("transparent").setup({
+			extra_groups = {
+				"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+			},
+			-- cursorがある行の背景を透過しない
+			exclude_groups = { "CursorLine" },
+		})
+		-- neo tree関係の背景を透過
+		require("transparent").clear_prefix("NeoTree")
+		-- lualine関係の背景を透過
+		-- require('transparent').clear_prefix('lualine')
+		-- icebergを使う時noiceのcommand paletteのfloat windowの背景を透過
+		require("transparent").clear_prefix("noice")
+		-- 常に透過を有効にする
+		vim.g.transparent_enabled = true
+	end,
+}
