80d1c6b639148bc5807c23d25897112ea73f732f
Kyohei Uto <h27.kubb@gmail.com>
2024-12-31 15:08:39 +0900

    keymap変更

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
 	event = "BufRead",
 	config = function()
 		require("toggleterm").setup({
-			open_mapping = [[<C-`>]], -- vscodeのkeybindに合わせる
+			open_mapping = [[<C-t>]],
 			start_in_insert = true,
 			insert_mappings = false, -- insert時はmapping適用しない(文字入力時にスペースの入力が遅くなるので)
 			direction = "float",
