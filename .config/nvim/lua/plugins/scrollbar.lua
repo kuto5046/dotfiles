06e2e7a4889ed3f55a6165c808fdea7c9c5ac4f7
Kyohei Uto <h27.kubb@gmail.com>
2025-01-01 09:53:40 +0900

    vscode対応をなくす

--- .config/nvim/lua/plugins/scrollbar.lua
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -1,12 +1,7 @@
--- -- vscodeで利用すると黒い点が表示されるので、vscodeの場合は読み込まない
-if vim.g.vscode then
-	return {}
-else
-	return {
-		"petertriho/nvim-scrollbar",
-		event = "BufRead",
-		config = function()
-			require("scrollbar").setup()
-		end,
-	}
-end
+return {
+	"petertriho/nvim-scrollbar",
+	event = "BufRead",
+	config = function()
+		require("scrollbar").setup()
+	end,
+}
