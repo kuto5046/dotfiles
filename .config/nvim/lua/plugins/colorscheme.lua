c2f98eb98bb3607f96501d8dff369a35cf3a1ce4
Kyohei Uto <h27.kubb@gmail.com>
2025-01-03 16:23:30 +0900

    catppuccin colorscheme設定を追加し、各種統合を有効化

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,25 @@
 return {
 	{ "EdenEast/nightfox.nvim" },
 	{ "cocopon/iceberg.vim" },
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					treesitter = true,
+					notify = true,
+					barbar = true,
+					diffview = true,
+					neotree = true,
+					neogit = true,
+					noice = true,
+					which_key = true,
+				},
+			})
+		end,
+	},
 }
