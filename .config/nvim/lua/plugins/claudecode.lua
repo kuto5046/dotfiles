c112837ede9192fbc09e768afee3c2e766824961
Kyohei Uto <h27.kubb@gmail.com>
2025-12-01 15:11:41 +0900

    fix

--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -20,4 +20,25 @@ return {
 		{ "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
 		{ "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
 	},
+	opts = {
+		terminal = {
+			---@module "snacks"
+			---@type snacks.win.Config|{}
+			snacks_win_opts = {
+				position = "float",
+				width = 0.9,
+				height = 0.9,
+				keys = {
+					claude_hide = {
+						toggle_key,
+						function(self)
+							self:hide()
+						end,
+						mode = "t",
+						desc = "Hide",
+					},
+				},
+			},
+		},
+	},
 }
