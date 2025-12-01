2420e362ff837037b5b9da6ca280f10cc01972c7
Kyohei Uto <h27.kubb@gmail.com>
2025-12-01 22:18:10 +0900

    weztermだと有効にしても良さそう

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -1,20 +1,18 @@
 -- cursor移動をかっこいいアニメーションにするプラグイン
--- かっこいいけど速度が遅く感じるのとカーソルがチラつくのでOFFにしている
 return {
-	-- "sphamba/smear-cursor.nvim",
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		-- Smear cursor when switching buffers or windows.
+		smear_between_buffers = true,
 
-	-- opts = {
-	-- 	-- Smear cursor when switching buffers or windows.
-	-- 	smear_between_buffers = true,
+		-- Smear cursor when moving within line or to neighbor lines.
+		smear_between_neighbor_lines = true,
 
-	-- 	-- Smear cursor when moving within line or to neighbor lines.
-	-- 	smear_between_neighbor_lines = false,
+		-- Draw the smear in buffer space instead of screen space when scrolling
+		scroll_buffer_space = true,
 
-	-- 	-- Draw the smear in buffer space instead of screen space when scrolling
-	-- 	scroll_buffer_space = false,
-
-	-- 	-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-	-- 	-- Smears will blend better on all backgrounds.
-	-- 	legacy_computing_symbols_support = false,
-	-- },
+		-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+		-- Smears will blend better on all backgrounds.
+		legacy_computing_symbols_support = false,
+	},
 }
