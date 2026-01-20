a6e7bea754cd1692bbf66f1e634154841b7eb944
Kyohei Uto <h27.kubb@gmail.com>
2026-01-20 23:00:03 +0900

    dap使ってないので削除

--- .config/nvim/lua/plugins/neodev.lua
+++
@@ -1,9 +0,0 @@
--- dap-uiで型チェックを有効にするために推奨されている
-return {
-	"folke/neodev.nvim",
-	config = function()
-		require("neodev").setup({
-			library = { plugins = { "nvim-dap-ui", "neotest" }, types = true },
-		})
-	end,
-}
