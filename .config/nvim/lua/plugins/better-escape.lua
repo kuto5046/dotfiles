8a4f3030e3a00ab539dd487c77951e686fc74c12
Kyohei Uto <h27.kubb@gmail.com>
2026-02-07 22:30:28 +0900

    fix

--- .config/nvim/lua/plugins/better-escape.lua
+++ .config/nvim/lua/plugins/better-escape.lua
@@ -1,13 +1,14 @@
-return {
-	"max397574/better-escape.nvim",
-	opts = {
-		default_mappings = false,
-		mappings = {
-			t = {
-				j = {
-					k = "<C-\\><C-n>",
-				},
-			},
-		},
-	},
-}
+return   {
+    "max397574/better-escape.nvim",
+    event = { "BufReadPre", "BufWritePre", "BufNewFile" },
+    opts = {
+      timeout = 100,
+      default_mappings = false,
+      mappings = {
+        i = { j = { k = "<ESC>" } },
+        t = {
+          j = { k = "<C-\\><C-n>" },
+        },
+      },
+    },
+  }
