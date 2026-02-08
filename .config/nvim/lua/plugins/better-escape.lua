346251f072467b2d91756414adeaeb0c40a51409
Kyohei Uto <h27.kubb@gmail.com>
2026-02-08 22:13:38 +0900

    escapeの読み込みタイミング修正

--- .config/nvim/lua/plugins/better-escape.lua
+++ .config/nvim/lua/plugins/better-escape.lua
@@ -1,14 +1,15 @@
 return {
-  "max397574/better-escape.nvim",
-  event = { "BufReadPre", "BufWritePre", "BufNewFile" },
-  opts = {
-    timeout = 100,
-    default_mappings = false,
-    mappings = {
-      i = { j = { k = "<ESC>" } },
-      t = {
-        j = { k = "<C-\\><C-n>" },
-      },
-    },
-  },
+	"max397574/better-escape.nvim",
+	event = { "InsertEnter", "CmdlineEnter", "TermOpen" },
+	opts = {
+		timeout = 100,
+		default_mappings = false,
+		mappings = {
+			i = { j = { k = "<ESC>" } },
+			c = { j = { k = "<ESC>" } },
+			t = {
+				j = { k = "<C-\\><C-n>" },
+			},
+		},
+	},
 }
